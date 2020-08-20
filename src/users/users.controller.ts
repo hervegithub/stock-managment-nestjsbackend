8e11dc514dd94e5957628920ebf09b93c5e4883c
hervegithub <herventonga20@gmail.com>
2020-08-20 01:41:00 +0100

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { Users } from './modals/users.interface';
+import { Users } from './users.model';
 
 @Controller('users')
 export class UsersController {
@@ -8,12 +8,28 @@ export class UsersController {
     constructor(private readonly userService:UsersService){}
 
     @Get()
-    getTest() : string {
-        return this.userService.testUsersAPi();
+    getAllCompany(){
+       return  this.userService.getAllCompany();
+    }
+
+    @Get(':id')
+    getCompanyById(@Param('id') id : string){
+       return  this.userService.getCompanyById(id);
     }
     
     @Post()
     postCompany(@Body() company: Users){
          return this.userService.addNewCompany(company);
+    }
+
+    @Delete()
+    removeCompany(@Param('id') id : string){
+        return this.userService.removeCompanyById(id);
+    }
+
+    @Patch(':id')
+    updateCompany(@Body() company: Users, @Param('id') id : string){
+        this.userService.updateCompany(company, id);
+        return null;
     }
 }
