67f5aad83564d8fea7869373511f0ffcb5cc5c72
hervegithub <herventonga20@gmail.com>
2020-08-20 01:41:04 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import {MongooseModule} from "@nestjs/mongoose"
+
+
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, MongooseModule.forRoot('mongodb+srv://codehero:TU1vxpN3yzEObuwk@cluster0-qjkfe.mongodb.net/gestion-stock?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
